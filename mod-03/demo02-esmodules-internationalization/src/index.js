11345aa5207e16c4d544f4d73850cff800e3e188
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-10-25 21:26:37 -0400

    refactor terminal class

--- mod-03/demo02-esmodules-internationalization/src/index.js
+++ mod-03/demo02-esmodules-internationalization/src/index.js
@@ -1,52 +1,32 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-import readline from "readline";
-
+import { TerminalController } from "./TerminalController.js";
 import database from "../database.json";
 import Person from "./person.js";
 
-DraftLog(console).addLineListener(process.stdin);
+const DEFAULT_LANG = "pt-BR";
+const STOP_TERMINAL = ":q";
 
-const DEFAULT_LANG = "en-ca";
+const terminalController = new TerminalController();
 
-const options = {
-  leftPad: 2,
-  columns: [
-    {
-      field: "id",
-      name: chalk.cyan("ID"),
-    },
-    {
-      field: "vehicles",
-      name: chalk.magenta("Vehicles"),
-    },
-    {
-      field: "kmTraveled",
-      name: chalk.cyan("Km Traveled"),
-    },
-    {
-      field: "from",
-      name: chalk.cyan("From"),
-    },
-    {
-      field: "to",
-      name: chalk.cyan("To"),
-    },
-  ],
-};
+terminalController.initializeTerminal(database, DEFAULT_LANG);
 
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-const print = console.draft(table);
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question("What?");
 
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    if (answer === STOP_TERMINAL) {
+      terminalController.closeTerminal();
+      console.log("Process finished!");
+      return;
+    }
 
-/* terminal.question("What's your name?", (msg) => {
-  console.log("msg", msg.toString());
-}); */
+    const person = Person.generateInstanceFromString(answer);
+    console.log("📍   person", person.formatted(DEFAULT_LANG));
+
+    return mainLoop();
+  } catch (error) {
+    console.log("An error occurred!");
+    return mainLoop();
+  }
+}
+
+await mainLoop();
