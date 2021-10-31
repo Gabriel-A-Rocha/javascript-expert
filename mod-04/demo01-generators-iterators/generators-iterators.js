1884130fdfac1853d690af7c474915223ad17868
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-10-31 01:38:52 -0400

    create async iterator

--- mod-04/demo01-generators-iterators/generators-iterators.js
+++ mod-04/demo01-generators-iterators/generators-iterators.js
@@ -22,4 +22,38 @@ deepStrictEqual(generator.next(), { value: undefined, done: true });
 deepStrictEqual(Array.from(main()), ["Hello", "-", "World", 200]);
 deepStrictEqual([...main()], ["Hello", "-", "World", 200]);
 
-console.log([...main()]);
+// Async Iterators
+
+const { readFile, stat, readdir } = require("fs/promises");
+
+function* promisified() {
+  yield readFile(__filename);
+  yield Promise.resolve("Hey Dude");
+}
+
+/* Promise.all([...promisified()]).then((results) => {
+  console.log(" 🔹 results", results.toString());
+}); */
+
+/* (async () => {
+  for await (const item of promisified()) {
+    console.log(" 🔹 item", item.toString());
+  }
+})(); */
+
+async function* systemInfo() {
+  const file = await readFile(__filename);
+  yield { file: file.toString() };
+
+  const { size } = await stat(__filename);
+  yield { size };
+
+  const dir = await readdir(__dirname);
+  yield { dir };
+}
+
+(async () => {
+  for await (const item of systemInfo()) {
+    console.log(" 🔹 item", item);
+  }
+})();
