693075cc3cc814bbf0e3303b5fbde89b09313a69
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-11-03 22:47:24 -0400

    create iterator using Symbol

--- mod-04/demo02-symbol/symbol.js
+++ mod-04/demo02-symbol/symbol.js
@@ -1,7 +1,6 @@
 const assert = require("assert");
 
-// Keys
-
+// Unique Keys
 const uniqueKey = Symbol("userName");
 
 const user = {};
@@ -12,3 +11,41 @@ user[uniqueKey] = "value for symbol";
 assert.deepStrictEqual(user["userName"], "value for ordinary objects");
 assert.deepStrictEqual(user[uniqueKey], "value for symbol");
 assert.deepStrictEqual(user[Symbol("userName")], undefined);
+
+assert.deepStrictEqual(Object.getOwnPropertySymbols(user)[0], uniqueKey);
+
+// Well Known Symbols
+
+const obj = {
+  [Symbol.iterator]: () => {
+    return {
+      items: ["c", "b", "a"],
+      next() {
+        return {
+          done: this.items.length === 0,
+          value: this.items.pop(),
+        };
+      },
+    };
+  },
+};
+
+for (let item of obj) {
+  console.log("item", item);
+}
+
+console.log([...obj]);
+
+assert.deepStrictEqual([...obj], ["a", "b", "c"]);
+
+const kItems = Symbol("kItems");
+
+class MyDate {
+  constructor(...args) {
+    this[kItems] = args.map((arg) => new Date(...arg));
+  }
+}
+
+const myDate = new MyDate([2020, 0, 3], [2020, 1, 8], [2020, 2, 12]);
+
+console.log("myDate", myDate);
