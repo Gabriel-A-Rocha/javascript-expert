e7422e950d0b44e8242e707dc0b2e659f34084cd
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-11-10 07:21:02 -0500

    add Reflect usage comparisons

--- mod-04/demo05-reflect/reflect.js
+++ mod-04/demo05-reflect/reflect.js
@@ -25,39 +25,57 @@ deepStrictEqual(result, 20);
 
 function myDate() {}
 
-// creating object property --> using Object.defineProperty
-Object.defineProperty(myDate, "prop1", { value: () => "Return 1!" });
-console.log("myDate.prop1", myDate.prop1());
+// creating object property
 
+// Object.defineProperty
+Object.defineProperty(myDate, "prop1", { value: () => "Return 1!" });
 deepStrictEqual(myDate.prop1(), "Return 1!");
 
-// creating object property --> using Reflect.defineProperty
+// Reflect.defineProperty
 Reflect.defineProperty(myDate, "prop2", { value: () => "Return 2!" });
-console.log("myDate.prop2", myDate.prop2());
-
 deepStrictEqual(myDate.prop2(), "Return 2!");
 
-// deleting object property --> using delete operator (not recommended)
+// deleting object property
+
+// delete operator (not recommended)
 const user1 = { name: "Gabe" };
-console.log("user1", user1);
-
 delete user1.name;
-console.log("user1", user1);
-
 deepStrictEqual(user1.hasOwnProperty("user1"), false);
 
-// deleting object property --> using Reflect
+// Reflect.deleteProperty
 const user2 = { name: "Mary" };
 Reflect.deleteProperty(user2, "name");
-console.log("user2", user2);
-
 deepStrictEqual(user2.hasOwnProperty("name"), false);
 
 const hero1 = { superman: "Clark Kent" };
 const hero2 = { batman: "Bruce Wayne" };
 
-// verify key existance --> using "in"
+// verify key existance
+
+// in operator
 ok("superman" in hero1);
 
-// verify key existance --> using Reflect
+// Reflect.has
 ok(Reflect.has(hero2, "batman"));
+
+// list object keys
+
+// Object.getOwnPropertyNames, Object.getOwnPropertySymbols
+const user = Symbol("user");
+const userObject = {
+  id: 1,
+  [Symbol.for("password")]: 123,
+  [user]: "Gabriel",
+};
+
+const objKeys = [
+  ...Object.getOwnPropertyNames(userObject),
+  ...Object.getOwnPropertySymbols(userObject),
+];
+deepStrictEqual(objKeys, ["id", Symbol.for("password"), user]);
+
+// Reflect.ownKeys
+deepStrictEqual(
+  JSON.stringify(Reflect.ownKeys(userObject)),
+  JSON.stringify(["id", Symbol("password"), Symbol("user")])
+);
