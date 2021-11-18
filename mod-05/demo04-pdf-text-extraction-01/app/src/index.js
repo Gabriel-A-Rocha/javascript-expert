ead0fadefcce462c28a21a02f8cd4112a1277323
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-11-17 22:19:47 -0500

    use facade pattern to hide text processing methods

--- mod-05/demo04-pdf-text-extraction-01/app/src/index.js
+++ mod-05/demo04-pdf-text-extraction-01/app/src/index.js
@@ -4,8 +4,15 @@ const { readFile, writeFile } = require("fs/promises");
 const { join } = require("path");
 const pdf = require("pdf-parse");
 
+const { TextProcessorFacade } = require("./textProcessorFacade");
+
 (async () => {
   const dataBuffer = await readFile(join(__dirname, "./../../../docs/contrato.pdf"));
   const data = await pdf(dataBuffer);
-  await writeFile(join(__dirname, "text.txt"), data.text.trim());
+  const text = data.text.trim();
+  await writeFile(join(__dirname, "text.txt"), text);
+
+  const instance = new TextProcessorFacade(text);
+  const people = instance.getPeopleFromPDF();
+  console.log("🪁   people", people);
 })();
