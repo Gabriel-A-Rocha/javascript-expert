92d4fa50c176af50860fc776fab24700785a4823
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-11-17 21:56:40 -0500

    add mapPerson method

--- mod-05/demo04-pdf-text-extraction-01/app/src/textProcessorFluentAPI.js
+++ mod-05/demo04-pdf-text-extraction-01/app/src/textProcessorFluentAPI.js
@@ -1,4 +1,5 @@
 const { evaluateRegex } = require("./util");
+const { Person } = require("../src/person");
 
 class TextProcessorFluentAPI {
   #content;
@@ -23,6 +24,11 @@ class TextProcessorFluentAPI {
   removeEmptyCharacters() {
     const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g);
     this.#content = this.#content.map((line) => line.map((item) => item.replace(trimSpaces, "")));
+    return this;
+  }
+
+  mapPerson() {
+    this.#content = this.#content.map((line) => new Person(line));
     return this;
   }
 
