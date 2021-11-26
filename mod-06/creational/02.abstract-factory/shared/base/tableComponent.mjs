64b52d2cd2bdce334818364b0abbb939e85dab1a
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-11-25 22:18:01 -0500

    implement abstract factory pattern for table rendering

--- mod-06/creational/02.abstract-factory/shared/base/tableComponent.mjs
+++ mod-06/creational/02.abstract-factory/shared/base/tableComponent.mjs
@@ -1,3 +1,8 @@
+import NotImplementedException from "../notImplementedException.mjs";
+
+// simulating and interface in JavaScript
 export default class TableComponent {
-  render(data) {}
+  render(data) {
+    throw new NotImplementedException(this.render.name);
+  }
 }
