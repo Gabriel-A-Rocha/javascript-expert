15e0b5d2ca5b0a6edd4ad90eb7217b44a7d1c9ad
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-11-26 22:12:34 -0500

    render table in browser and console using abstract factory

--- mod-06/creational/02.abstract-factory/platforms/browser/table.mjs
+++ mod-06/creational/02.abstract-factory/platforms/browser/table.mjs
@@ -2,6 +2,29 @@ import TableComponent from "../../shared/base/tableComponent.mjs";
 
 export default class TableBrowserComponent extends TableComponent {
   render(data) {
-    window.alert("Rendering on browser!");
+    const template = this.prepareDate(data);
+    document.querySelector("body").innerHTML = template;
+  }
+
+  prepareDate(data) {
+    const [firstItem] = data;
+    const tHeaders = Object.keys(firstItem).map((text) => `<th scope="col">${text}</th>`);
+
+    const joinLists = (list) => list.join("");
+    const tBodyValues = data
+      .map((item) => Object.values(item))
+      .map((item) => item.map((value) => `<td>${value}</td>`))
+      .map((tds) => `<tr>${joinLists(tds)}</tr>`);
+
+    const template = `
+        <table class="table table-striped table-hover">
+          <thead>
+              <tr>${joinLists(tHeaders)}</tr>
+          </thead>
+          <tbody>${joinLists(tBodyValues)}</tbody>
+      </table>
+    `;
+
+    return template;
   }
 }
--- mod-06/creational/02.abstract-factory/platforms/console/table.mjs
+++ mod-06/creational/02.abstract-factory/platforms/console/table.mjs
@@ -2,6 +2,6 @@ import TableComponent from "../../shared/base/tableComponent.mjs";
 
 export default class TableConsoleComponent extends TableComponent {
   render(data) {
-    console.log("Rendering on console!", data);
+    console.log("Rendering on console!\n", data);
   }
 }
