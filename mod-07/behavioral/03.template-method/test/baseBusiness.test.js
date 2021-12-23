3357f063c5bcdd97bb6a3ffbee26cfccd3ebb13a
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-12-22 22:28:21 -0500

    add test scenarios for base class

--- mod-07/behavioral/03.template-method/test/baseBusiness.test.js
+++ mod-07/behavioral/03.template-method/test/baseBusiness.test.js
@@ -11,9 +11,38 @@ describe("BaseBusiness Abstract Class", () => {
     expect(() => concreteClass.create({})).toThrow(validationError);
   });
 
-  it.todo("should throw an error when _validateRequiredFields returns false");
+  it("should throw an error when _validateRequiredFields returns false", () => {
+    class ConcreteClass extends BaseBusiness {
+      _validateRequiredFields = jest.fn().mockReturnValue(false);
+    }
+    const concreteClass = new ConcreteClass();
 
-  it.todo("should throw if child class does not implement _create method");
+    const validationError = new Error("Invalid data");
+    expect(() => concreteClass.create({})).toThrow(validationError);
+  });
 
-  it.todo("should call _validateRequiredFields and _create on create");
+  it("should throw if child class does not implement _create method", () => {
+    class ConcreteClass extends BaseBusiness {
+      _validateRequiredFields = jest.fn().mockReturnValue(true);
+    }
+    const concreteClass = new ConcreteClass();
+
+    const createMethodError = new NotImplementedException(concreteClass._create.name);
+    expect(() => concreteClass.create({})).toThrow(createMethodError);
+  });
+
+  it("should call _validateRequiredFields and _create on create", () => {
+    class ConcreteClass extends BaseBusiness {
+      _validateRequiredFields = jest.fn().mockReturnValue(true);
+      _create = jest.fn().mockReturnValue(true);
+    }
+    const concreteClass = new ConcreteClass();
+    const baseClassCreateSpy = jest.spyOn(BaseBusiness.prototype, BaseBusiness.prototype.create.name);
+
+    const result = concreteClass.create({});
+    expect(result).toBeTruthy();
+    expect(baseClassCreateSpy).toHaveBeenCalled();
+    expect(concreteClass._validateRequiredFields).toHaveBeenCalled();
+    expect(concreteClass._create).toHaveBeenCalled();
+  });
 });
