dd3d4b66b442230cc30c37308e3eae7b79c6843a
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-12-22 21:08:52 -0500

    scaffold template method project

---
+++ mod-07/behavioral/03.template-method/src/business/base/baseBusiness.js
@@ -0,0 +1,17 @@
+import { NotImplementedException } from "../../util/exceptions.js";
+
+export default class BaseBusiness {
+  _validateRequiredFields(data) {
+    throw new NotImplementedException("_validateRequiredFields");
+  }
+
+  _create(data) {
+    throw new NotImplementedException("_create");
+  }
+
+  create(data) {
+    const isValid = this._validateRequiredFields(data);
+    if (!isValid) throw new Error("Invalid data");
+    return this._create(data);
+  }
+}
---
+++ mod-07/behavioral/03.template-method/src/util/exceptions.js
@@ -0,0 +1,9 @@
+class NotImplementedException extends Error {
+  constructor(functionName) {
+    super(`${functionName} was called without an implementation`);
+
+    this.name = "NotImplementedException";
+  }
+}
+
+export { NotImplementedException };
