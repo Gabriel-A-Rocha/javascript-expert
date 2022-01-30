db50dd2a90d6f5132cef750eb655719246140f17
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-01-30 11:43:38 -0500

    create MongoDB connection method

--- mod-12/02-graceful-shutdown/index.js
+++ mod-12/02-graceful-shutdown/index.js
@@ -1,1 +1,34 @@
 import { MongoClient } from "mongodb";
+
+async function dbConnect() {
+  // connection URL
+  const url = "mongodb://localhost:27017";
+  const client = new MongoClient(url);
+
+  await client.connect();
+  console.log("Connected to MongoDB server");
+
+  // database name
+  const dbName = "comics";
+
+  const db = client.db(dbName);
+  return {
+    collections: {
+      heroes: db.collection("heroes"),
+      client,
+    },
+  };
+}
+
+try {
+  const { collections, client } = await dbConnect();
+
+  await collections.heroes.insertOne({
+    name: "Flash",
+    updatedAt: new Date().toISOString(),
+  });
+
+  const heroes = await collections.heroes.find({}).toArray();
+} catch (error) {
+  console.log(error);
+}
