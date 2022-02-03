06ce2318369a213139f1b9b79791860aa0ffa03b
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-02-02 22:40:13 -0500

    create web server

--- mod-12/02-graceful-shutdown/index.js
+++ mod-12/02-graceful-shutdown/index.js
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import { createServer } from "http";
 
 async function dbConnect() {
   // connection URL
@@ -21,12 +22,28 @@ async function dbConnect() {
 const { collections, client } = await dbConnect();
 
 async function handler(request, response) {
-  await collections.heroes.insertOne({
-    name: "Flash",
-    updatedAt: new Date().toISOString(),
-  });
-  const heroes = await collections.heroes.find({}).toArray();
-  console.log("\n🪁 ~ heroes", heroes);
+  for await (const data of request) {
+    try {
+      const hero = JSON.parse(data);
+      await collections.heroes.insertOne({
+        ...hero,
+        updatedAt: new Date().toISOString(),
+      });
+      const heroes = await collections.heroes.find({}).toArray();
+      response.writeHead(201);
+      response.write(JSON.stringify(heroes));
+    } catch (error) {
+      console.log("Error with the incoming request", error);
+      response.writeHead(500);
+      response.write(JSON.stringify({ message: "Internal server error" }));
+    } finally {
+      response.end();
+    }
+  }
 }
 
-client.close();
+//client.close();
+
+const server = createServer(handler).listen(3000, () =>
+  console.log({ status: "Server listening at port 3000", processId: process.pid })
+);
