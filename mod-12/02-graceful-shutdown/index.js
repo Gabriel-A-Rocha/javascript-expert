89486257fee79da810c48581a792e38281ff0945
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-02-02 22:16:21 -0500

    create handler method for db insertion

--- mod-12/02-graceful-shutdown/index.js
+++ mod-12/02-graceful-shutdown/index.js
@@ -4,31 +4,29 @@ async function dbConnect() {
   // connection URL
   const url = "mongodb://localhost:27017";
   const client = new MongoClient(url);
-
   await client.connect();
   console.log("Connected to MongoDB server");
 
-  // database name
   const dbName = "comics";
-
+  const collectionName = "heroes";
   const db = client.db(dbName);
   return {
     collections: {
-      heroes: db.collection("heroes"),
-      client,
+      heroes: db.collection(collectionName),
     },
+    client,
   };
 }
 
-try {
-  const { collections, client } = await dbConnect();
+const { collections, client } = await dbConnect();
 
+async function handler(request, response) {
   await collections.heroes.insertOne({
     name: "Flash",
     updatedAt: new Date().toISOString(),
   });
-
   const heroes = await collections.heroes.find({}).toArray();
-} catch (error) {
-  console.log(error);
+  console.log("\n🪁 ~ heroes", heroes);
 }
+
+client.close();
