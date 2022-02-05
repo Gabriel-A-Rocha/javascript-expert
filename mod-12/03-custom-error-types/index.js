aaa049be533473169ebcbdbf3cd1ca4ce4c89f00
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-02-04 22:30:13 -0500

    Create web server

---
+++ mod-12/03-custom-error-types/index.js
@@ -0,0 +1,17 @@
+import { createServer } from "http";
+
+async function handler(request, response) {
+  for await (const data of request) {
+    try {
+      const hero = JSON.parse(data);
+      console.log("\n🪁 ~ hero ", hero);
+      response.writeHead(200);
+      response.end();
+    } catch (error) {
+      response.writeHead(500);
+      response.end();
+    }
+  }
+}
+
+createServer(handler).listen(3000, () => console.log("Server listening at port 3000"));
