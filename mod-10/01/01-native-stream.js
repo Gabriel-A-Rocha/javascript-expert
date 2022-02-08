63ff81417e09f3d86534e40356946eaa7c1b2bb4
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2022-02-07 22:09:07 -0500

    add readStream example

--- mod-10/01/01-native-stream.js
+++ mod-10/01/01-native-stream.js
@@ -14,3 +14,20 @@ import process from "process";
 
 // terminal 2 - Client
 // node -e "process.stdin.pipe(require('net').connect(1338))"
+
+// node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
+
+import http from "http";
+import { createReadStream, readFileSync } from "fs";
+
+http
+  .createServer((req, res) => {
+    // bad practice
+    // const file = readFileSync("big.file").toString();
+
+    createReadStream("big.file").pipe(res);
+
+    res.statusCode = 200;
+    res.end();
+  })
+  .listen(3000, () => console.log("Running at port 3000"));
